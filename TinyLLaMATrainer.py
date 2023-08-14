0b260b38da50a604e23027944d13b71ff740cdb9
rkandas <53561723+rkandas@users.noreply.github.com>
2023-08-14 15:26:36 +0530

    [Raju]- sentence trainer

--- TinyLLaMATrainer.py
+++ TinyLLaMATrainer.py
@@ -19,11 +19,11 @@ class TinyLLaMATrainer:
         self.start_index = 0
         self.device = device
 
-        self.block_size = 512
+        self.block_size = 1024
         self.batch_size = 2
-        self.max_iters = 200000
-        self.eval_interval = 1000
-        self.log_interval = 10
+        self.max_iters = 100000
+        self.eval_interval = 100
+        self.log_interval = 2
         self.start_index = 0
         self.grad_clip = 1.5
         self.out_dir = "out/training"
@@ -31,7 +31,7 @@ class TinyLLaMATrainer:
         self.weight_decay = 0.01
         self.accumulation_steps = 4
 
-        self.strategy = SingleDeviceStrategy(device="cuda:0", accelerator="cuda")
+        self.strategy = SingleDeviceStrategy(device="mps", accelerator="mps")
         self.fabric = L.Fabric(devices=1, precision="bf16-mixed", strategy=self.strategy)
         self.writer = SummaryWriter(log_dir='out/logs')
 
@@ -281,8 +281,9 @@ class TinyLLaMATrainer:
 
 
 def init_device_and_fabric():
-    device = "cuda" if torch.has_cuda else exit("No cuda detected. Aborting the training.")
-    strategy = SingleDeviceStrategy(device="cuda:0", accelerator="cuda")
+    #device = "cuda" if torch.has_cuda else exit("No cuda detected. Aborting the training.")
+    device = "mps"
+    strategy = SingleDeviceStrategy(device="mps:0", accelerator="mps")
     fabric = L.Fabric(devices=1, precision="bf16-mixed", strategy=strategy)
     writer = SummaryWriter(log_dir='out/logs')
     fabric.seed_everything(1337 + fabric.global_rank)
